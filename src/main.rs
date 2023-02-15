384ca23b6a4f9dd81e68059a9ddbf07f1610ba14
hend0x <sinclair500@gmail.com>
2023-02-15 10:07:21 +0000

    add test data and tests

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,23 @@ fn main() {
         .about("Rust echo")
         .arg(
             Arg::with_name("text")
-            .value_name("TEXT")
-            .help("Input text")
-            .required(true)
-            .min_values(1),
-            )
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
         .arg(
             Arg::with_name("omit_newline")
-            .short("n")
-            .help("Do not print newline")
-            .takes_value(false)
-            )
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
         .get_matches();
-    println!("{:#?}", matches);
+
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+
+    let ending = if omit_newline { "" } else { "\n" };
+
+    print!("{}{}", text.join(" "), ending)
 }
